083125139eb1c0c3c781ef516bd74e5d5a4959c6
anna_kovalenko_4 <notmelife@mail.ru>
2025-05-10 20:22:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import SessionLocal, init_db
+from models import Recipe, RecipeCreate, RecipeResponse
+
+app = FastAPI()
+
+init_db()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/recipes", response_model=list[RecipeResponse])
+def read_recipes(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    """
+    Получить список всех рецептов.
+
+    - **skip**: Количество пропускаемых записей (для пагинации).
+    - **limit**: Максимальное количество возвращаемых записей.
+
+    Рецепты сортируются по количеству просмотров и времени приготовления.
+    """
+    return db.query(Recipe).order_by(Recipe.views.desc(), Recipe.cooking_time).offset(skip).limit(limit).all()
+
+
+@app.get("/recipes/{recipe_id}", response_model=RecipeResponse)
+def read_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    """
+    Получить детальную информацию о конкретном рецепте по его ID.
+
+    - **recipe_id**: ID рецепта.
+
+    Возвращает информацию о названии блюда,
+    времени приготовления,
+    списке ингредиентов и текстовом описании.
+    """
+    recipe = db.query(Recipe).filter(Recipe.id == recipe_id).first()
+
+    if recipe is None:
+        raise HTTPException(status_code=404, detail="Рецепт не найден")
+
+    return recipe
+
+
+@app.post("/recipes", response_model=RecipeResponse)
+def create_recipe(recipe_create: RecipeCreate, db: Session = Depends(get_db)):
+    """
+    Создать новый рецепт.
+
+    - **recipe**: Объект нового рецепта с полями title,
+      cooking_time, ingredients и description.
+
+    Возвращает созданный рецепт с его ID.
+    """
+
+    recipe = Recipe(**recipe_create.dict())
+
+    db.add(recipe)
+    db.commit()
+    db.refresh(recipe)
+
+    return recipe
---
+++ models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, Text
+from database import Base
+from pydantic import BaseModel
+
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    views = Column(Integer, default=0)
+    cooking_time = Column(Integer)
+    ingredients = Column(Text)
+    description = Column(Text)
+
+    def __repr__(self):
+        return f"<Recipe(title={self.title}, views={self.views}, cooking_time={self.cooking_time})>"
+
+class RecipeCreate(BaseModel):
+    title: str
+    cooking_time: int
+    ingredients: str
+    description: str
+
+class RecipeResponse(RecipeCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
